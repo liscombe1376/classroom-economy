e5cac8c115e86ff7efba6aac9a9a14b38479dbc0
liscombe1376 <aliscombe@hpedsb.on.ca>
2026-05-10 21:31:06 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is the ONE place you update your URL
 const GLOBAL_CONFIG = {
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzjlVhWgBsMSbqUCNphMrJ9Al_jEwB9X0nDb-b4TjP9RjfjFhQhSzbgHRiuh-_mVaKI/exec"
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyz0yZlqYmUqkgXRI7h368PXJi3OnkQhJYi0Du4oKVILJdDQPAdIxCgYu7oGj8uCEE/exec"
 };
